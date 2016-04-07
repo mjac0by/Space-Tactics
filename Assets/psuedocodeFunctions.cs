bced40085ecdef93d5317fdb043f26aa52e33694
Ash Morrocco <ashmorrocco@gmail.com>
2016-04-06 23:05:13 -0400

    Remove Library files, Add Pseudocode Script
    Stop tracking /library

---
+++ Assets/psuedocodeFunctions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class psuedocodeFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+/* http://www.redblobgames.com/grids/hexagons/#coordinates
+ * Get position of ship (0,0)
+ * Get heading of ship (Deg) 30,90,150,210,270,330,390*
+ * for (# of moves){
+ * 	Calculate colors for heading 0, turn 60, turn -60
+ * 	if not already colored, apply colors
+ * }
+ * 
+*/
