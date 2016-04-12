e8b2d2bb4d23a7826a038bf8f0425c01f34eb331
mjac0by <astralcataclysm@gmail.com>
2016-04-12 16:35:47 -0400

    Minor changes and pseudocode
    Hexes default to and revert to gray
    L-click makes them green
    R-click makes them blue
    
    Pseudocode groundwork for players and ships

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Player
+{
+    class Player
+    {
+        //Score
+        public int kills, deaths, points;
+
+        //Ships controlled by this player
+        public List <Ship> fleet; //
+
+        //Any custom controls or camera settings
+    }
+}
---
+++ Assets/Scripts/Player/Ship.cs
@@ -0,0 +1,55 @@
+﻿namespace Player
+{
+    internal class Ship
+    {
+        private int hp, maxHP; //Hull
+        private int ap, maxAP; //Armor
+        private int sp, maxSP; //Shields
+        private int ep, maxEP; //Energy
+
+        private int facing; //Measure facing 0-5 clockwise with 0 being North
+                            //ie Z rotation is 60 degrees * facing relative to Y+ axis
+        private Point location;
+        private int momentum;
+
+        public Ship ()      //Default ship for testing
+        {
+            maxHP = 1; hp = maxHP;
+            maxAP = 0; ap = maxAP;
+            maxSP = 1; sp = maxSP;
+            maxEP = 1; ep = maxEP;
+
+            facing = 0;
+            location = new Point(0,0);
+        }
+
+        void takeDamage (int dmg, Point source, string dmgType)
+        {
+            //later, when it matters: determine direction of impact
+                //impact direction in radians relative to x axis = tan^-1 (source - location)
+                //convert to degrees; = (x*180/pi) % 360
+                //convert to facing; =floor( (x-90)/60 )
+            
+            //Apply damage one layer at a time until all damage is spent
+            //May be a multiplier based on weapon type
+            //Discard fractional damage but minimum 1 dmg on a successful hit
+                /* Shields
+                 * Armor
+                 * Hull
+                 */
+
+            //later, if you took hull damage, systems potentially go offline
+            //If so, apply debuffs as appropriate
+
+            //If hp=0, ship destroyed
+            //Later, on larger ships, we may have multiple hull sections, where if a section reaches 0, it means a hull breach
+                //Then we may consider crew death, repairs, and what the threshold of death is
+
+            //If a ship is destroyed
+                //Explosion sound/animation
+                //Remove ship from map and fleet
+                //Alter scoreboard, giving kills, deaths, and points as appropriate
+                //Create debris on the map. Potentially give that debris momentum.
+        }
+    }
+}
